83f93e11f54135cc542c917e06d2a9364ceb20c3
Joovski <53435623+Joovski@users.noreply.github.com>
2022-01-02 19:03:55 +0100

    Update inf.lua

--- inf.lua
+++ inf.lua
@@ -1,4 +1,5 @@
 game:GetService"RunService".RenderStepped:Connect(function()
-game:GetService("Players").LocalPlayer.Backpack.ammoTextBox.Text.MaxAmmo.Value = math.huge
-game:GetService("Players").LocalPlayer.Backpack.ammoTextBox.Text.ClipSize.Value = math.huge
+_G.GunName = ammoTextBox.Text
+game:GetService("Players").LocalPlayer.Backpack[_G.GunName].MaxAmmo.Value = math.huge
+game:GetService("Players").LocalPlayer.Backpack[_G.GunName].ClipSize.Value = math.huge
 end)
