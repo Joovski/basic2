5972277688e9fa5b1f4dfc07f5f856d97a994707
Joovski <53435623+Joovski@users.noreply.github.com>
2022-01-02 19:21:09 +0100

    Update inf.lua

--- inf.lua
+++ inf.lua
@@ -1,4 +1,4 @@
 game:GetService"RunService".RenderStepped:Connect(function()
-game:GetService("Players").LocalPlayer.Character."ammoTextBox.Text".MaxAmmo.Value = 100
-game:GetService("Players").LocalPlayer.Character."ammoTextBox.Text".ClipSize.Value = 100
+game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool").MaxAmmo.Value = math.huge
+game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool").ClipSize.Value = math.huge
 end)
