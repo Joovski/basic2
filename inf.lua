cc9a10b97762a80ac063a869d87674ca65768105
Joovski <53435623+Joovski@users.noreply.github.com>
2022-01-03 17:46:01 +0100

    Update inf.lua

--- inf.lua
+++ inf.lua
@@ -2,3 +2,19 @@ game:GetService"RunService".RenderStepped:Connect(function()
 game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool").MaxAmmo.Value = math.huge
 game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool").ClipSize.Value = math.huge
 end)
+
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/Joovski/notification/main/notif.lua"))()
+
+Notification.Notify("Paypal", "If you want to support me : PayPal.me/matenintraore", "rbxassetid://8327950081", {
+Duration = 6,       
+Main = {
+    Rounding = true,
+}
+});
+
+Notification.Notify("Yo", "Subscribe To HemLish For More Free and OP Feature", "rbxassetid://8327950081", {
+Duration = 6,       
+Main = {
+    Rounding = true,
+}
+});
