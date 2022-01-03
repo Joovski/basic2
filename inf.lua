ecd1e69dbd477cc01b9dfb957b52fe7f14d24160
Joovski <53435623+Joovski@users.noreply.github.com>
2022-01-03 17:58:38 +0100

    Update inf.lua

--- inf.lua
+++ inf.lua
@@ -6,14 +6,14 @@ end)
 local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/Joovski/notification/main/notif.lua"))()
 
 Notification.Notify("Paypal", "If you want to support me : PayPal.me/matenintraore", "rbxassetid://8327950081", {
-Duration = 6,       
+Duration = 10,       
 Main = {
     Rounding = true,
 }
 });
 
 Notification.Notify("Yo", "Subscribe To HemLish For More Free and OP Feature", "rbxassetid://8327950081", {
-Duration = 6,       
+Duration = 10,       
 Main = {
     Rounding = true,
 }
