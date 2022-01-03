a2437ed93f654e428269653e1dc26de9078678a2
Joovski <53435623+Joovski@users.noreply.github.com>
2022-01-03 17:59:01 +0100

    Update principal.lua

--- principal.lua
+++ principal.lua
@@ -26,3 +26,19 @@ Title = "Silent Aim"; -- title of notification
 Text = "Activated"; -- bottom text
 Duration = 5; -- how long notification is on screen
 })
+
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/Joovski/notification/main/notif.lua"))()
+
+Notification.Notify("Paypal", "If you want to support me : PayPal.me/matenintraore", "rbxassetid://8327950081", {
+Duration = 10,       
+Main = {
+    Rounding = true,
+}
+});
+
+Notification.Notify("Yo", "Subscribe To HemLish For More Free and OP Feature", "rbxassetid://8327950081", {
+Duration = 10,       
+Main = {
+    Rounding = true,
+}
+});
