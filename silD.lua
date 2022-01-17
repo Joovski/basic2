206acee73751415a020eca3ae6fe850be8028745
Joovski <53435623+Joovski@users.noreply.github.com>
2022-01-17 18:23:40 +0100

    Update silD.lua

--- silD.lua
+++ silD.lua
@@ -176,25 +176,3 @@ oldIndex = hookmetamethod(game, "__index", function(self, Index)
     
     return oldIndex(self, Index)
 end)
-
-game.StarterGui:SetCore("SendNotification", {
-Title = "Silent Aim"; -- title of notification
-Text = "Stopped"; -- bottom text
-Duration = 5; -- how long notification is on screen
-})
-
-wait(1.2)
-game.StarterGui:SetCore("SendNotification", {
-Title = "Information"; -- the title (ofc)
-Text = "Subscribe To HemLish For More Free and OP Feature"; -- what the text says (ofc)
-Icon = "rbxassetid://8327950081"; -- the image if u want. 
-Duration = 10; -- how long the notification should in secounds
-})
-
-wait(1.2)
-game.StarterGui:SetCore("SendNotification", {
-Title = "PayPal"; -- the title (ofc)
-Text = "If you want to support me : PayPal.me/matenintraore"; -- what the text says (ofc)
-Icon = "rbxassetid://8327950081"; -- the image if u want. 
-Duration = 10; -- how long the notification should in secounds
-})
